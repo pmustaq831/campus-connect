6fc94fbbe052fc029a9e3ed38f90e53e1438ba17
Your Name <you@example.com>
2026-02-22 21:26:31 +0530

    Fix PORT for deployment

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,8 @@ mongoose.connect(process.env.MONGO_URI)
   .catch((err) => console.log("DB Error:", err));
 
 // Start Server
-const PORT = 5000;
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
